f349c93886492cc9ec420ca0090e87b32361313f
Kevin Gorham <anothergmale@gmail.com>
2018-10-29 20:22:22 -0400

    revert to a basic drawer app

--- zcash-android-wallet-app/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-//Explicitly set `rootProject.buildFileName`
-// avoids project import issues caused by Android Studio silently adding a `build.gradle` file
-rootProject.buildFileName = "build.gradle.kts"
-
-include(":app", ":zcash-android-welding")
