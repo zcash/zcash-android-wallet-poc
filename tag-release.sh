2668970b118e23d85d89312265c8096f4538f16b
Kevin Gorham <anothergmale@gmail.com>
2018-12-11 14:19:58 -0500

    version bump

--- tag-release.sh
+++ tag-release.sh
@@ -7,8 +7,10 @@ read -p "TAG the release as above? Press y to tag ENTER to skip: [N] " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
    git tag -a v$(grep -oE 'versionName.*' zcash-android-wallet-app/app/build.gradle | sed 's/[^0-9]*\([0-9].*\w\).*/\1/') -m "Release on $(date)"
+   echo
    echo "build tagged"
 else
+   echo
    echo "tag not created!"   
 fi
 
@@ -22,9 +24,11 @@ then
    echo "Tags pushed, build triggered. Opening bitrise to verify..."
    xdg-open "https://app.bitrise.io/app/3f9040b242d98534#/builds"
 else
+   echo
    echo "tag not pushed!"
 fi
 
 echo
-
+echo "Done!"
+echo
 
