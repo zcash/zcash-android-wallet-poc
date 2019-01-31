77931bf1438ec47fd40d400b503b0f613fd2734a
Kevin Gorham <anothergmale@gmail.com>
2019-01-31 18:17:29 -0500

    add multidex because the bitrise build is failing without it.
    Local builds work fine without multidex so hopefully we can get away without adding it, later.

--- zcash-android-wallet-app/app/src/main/java/cash/z/android/wallet/ZcashWalletApplication.kt
+++ zcash-android-wallet-app/app/src/main/java/cash/z/android/wallet/ZcashWalletApplication.kt
@@ -1,9 +1,12 @@
 package cash.z.android.wallet
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import cash.z.android.wallet.di.component.DaggerApplicationComponent
 import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+
 
 class ZcashWalletApplication : DaggerApplication() {
 
@@ -18,6 +21,11 @@ class ZcashWalletApplication : DaggerApplication() {
      */
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.builder().create(this)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
     companion object {
