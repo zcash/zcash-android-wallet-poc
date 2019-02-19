edef5372ee46825c70c37600a47f617e12a57d04
Kevin Gorham <anothergmale@gmail.com>
2019-02-19 14:06:36 -0500

    about screen is feature complete

--- zcash-android-wallet-app/app/src/main/java/cash/z/android/wallet/ui/fragment/SettingsFragment.kt
+++ zcash-android-wallet-app/app/src/main/java/cash/z/android/wallet/ui/fragment/SettingsFragment.kt
@@ -1,12 +1,34 @@
 package cash.z.android.wallet.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import cash.z.android.wallet.R
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import cash.z.android.wallet.databinding.FragmentSettingsBinding
 
 
-class SettingsFragment : PlaceholderFragment()
+class SettingsFragment : BaseFragment() {
+    lateinit var binding: FragmentSettingsBinding
 
-@Module
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return DataBindingUtil
+            .inflate<FragmentSettingsBinding>(inflater, R.layout.fragment_settings, container, false)
+            .also { binding = it }
+            .root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mainActivity.setToolbarShown(true)
+    }
+
+}
+
+    @Module
 abstract class SettingsFragmentModule {
     @ContributesAndroidInjector
     abstract fun contributeSettingsFragment(): SettingsFragment
